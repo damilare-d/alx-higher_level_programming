6667f539695b9c95a13fb6c90f0340c0575de414
Akinwale Damilare <damilareakinwale0@gmail.com>
2022-12-10 04:34:47 +0100

    best score

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
