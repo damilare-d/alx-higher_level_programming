4c95ada4b0dcfcd9f902b2a64e297d5653a4192e
Akinwale Damilare <damilareakinwale0@gmail.com>
2023-01-03 17:42:49 +0100

    task still getting done oo

---
+++ 0x09-python-everything_is_object/12-answer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+l1 = [1,2,3]
+l2 = l1
+print(l1 == l2)
+
---
+++ 0x09-python-everything_is_object/15-answer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def assign_value(n,v):
+    n = v
+
+l = [1,2,3]
+m = [4,5.6]
+assign_value(l,m)
+print(l) 
